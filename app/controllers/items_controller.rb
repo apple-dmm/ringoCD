339a0ca30d05c3f935ec6018b08098e8abf596e3
suzukiarisa <ariko0325@gmail.com>
2019-11-20 06:23:47 +0000

    create-items-index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,15 +6,7 @@ class ItemsController < ApplicationController
   	 @q = Item.ransack(params[:q])
     @q.build_condition if @q.conditions.empty?
     @items = @q.result(distinct: true).page(params[:page]).per(30)
-  end
+     #@items = Item.all
 
-   def autocomplete_artist
-    artist_suggestions = Artist.autocomplete(params[:term]).pluck(:name)
-    respond_to do |format|
-      format.html
-      format.json{
-        render json: artist_suggestions
-      }
-    end
   end
 end
